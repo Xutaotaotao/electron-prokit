baf4c28ab68dc53633f01a3b55fe331bc07e1a0b
Xutaotaotao <xutaotao1108@outlook.com>
2023-09-26 10:30:10 +0800

    fix:pnpmResolve

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,15 @@
 const path = require("node:path");
 const ts = require("rollup-plugin-typescript2");
 const resolvePlugin = require("@rollup/plugin-node-resolve");
+const { pnpmResolve } = "@pnpm/resolve";
 const packagesDir = path.resolve(__dirname, "./packages");
 
 const globals = {
-  'koffi': 'koffi',
-  'axios': 'axios'
-}
+  koffi: "koffi",
+  axios: "axios",
+};
 
-function getBuildConfig(name,inputPath="src/index.ts") {
+function getBuildConfig(name, inputPath = "src/index.ts") {
   const packageDir = path.resolve(packagesDir, name);
   return {
     input: path.resolve(packageDir, inputPath),
@@ -16,18 +17,18 @@ function getBuildConfig(name,inputPath="src/index.ts") {
       {
         file: path.resolve(packageDir, `dist/${name}.cjs.js`),
         format: "cjs",
-        globals
+        globals,
       },
       {
         file: path.resolve(packageDir, `dist/${name}.mjs`),
-        format: 'es',
-        globals
+        format: "es",
+        globals,
       },
       {
         file: path.resolve(packageDir, `dist/${name}.umd.js`),
-        format: 'umd',
+        format: "umd",
         name,
-        globals
+        globals,
       },
     ],
     plugins: [
@@ -35,29 +36,30 @@ function getBuildConfig(name,inputPath="src/index.ts") {
         tsconfig: path.resolve(packageDir, "./tsconfig.json"),
       }),
       resolvePlugin({
-        moduleDirectories:['node_modules','.pnpm']
+        moduleDirectories: ["node_modules", ".pnpm"],
       }),
+      pnpmResolve(),
     ],
-    external:[
-       'electron',
-       'esbuild',
-       'postcss',
-       'koffi',
-       'globby',
-       'commander',
-       'restore-cursor',
-       'string-width',
-       'stdin-discarder',
-       'ora',
-       'pacote',
-       'axios'
+    external: [
+      "electron",
+      "esbuild",
+      "postcss",
+      "koffi",
+      "globby",
+      "commander",
+      "restore-cursor",
+      "string-width",
+      "stdin-discarder",
+      "ora",
+      "pacote",
+      "axios",
     ],
   };
 }
 
 module.exports = () => {
   return [
-    getBuildConfig('create-electron-prokit'),
-    getBuildConfig('electron-prokit')
+    getBuildConfig("create-electron-prokit"),
+    getBuildConfig("electron-prokit"),
   ];
 };
