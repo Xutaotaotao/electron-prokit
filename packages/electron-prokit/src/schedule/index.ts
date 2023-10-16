43635ec08613ccb69c2c29588492dffbbb00672b
Xutaotaotao <xutaotao1108@outlook.com>
2023-10-16 17:45:09 +0800

    feat: add Coveralls

--- packages/electron-prokit/src/schedule/index.ts
+++ packages/electron-prokit/src/schedule/index.ts
@@ -1,5 +1,5 @@
 interface RegisterScheduleOption {
-  name:string;
+  name: string;
   fun: Function;
   interval: number;
   [key: string]: any;
@@ -10,89 +10,88 @@ interface Schedule extends RegisterScheduleOption {
   instance?: any
 }
 
-const schedules = new Map<string,Schedule>()
+const schedules = new Map<string, Schedule>()
 
-function registerSchedule (schedule:RegisterScheduleOption):Schedule {
+function registerSchedule(schedule: RegisterScheduleOption): Schedule {
   if (hasSchedule(schedule.name)) {
     const thisSchedule = getSchedule(schedule.name)
-    if (thisSchedule.isRunning) {
+    if (thisSchedule?.isRunning) {
       console.warn("has same name schedule and isRunning");
       return thisSchedule
     } else {
-      schedules.set(schedule.name,schedule)
+      schedules.set(schedule.name, schedule)
       return schedule
     }
   } else {
-    schedules.set(schedule.name,schedule)
+    schedules.set(schedule.name, schedule)
     return schedule
   }
 }
 
-function hasSchedule (name:string):boolean {
+function hasSchedule(name: string): boolean {
   return schedules.has(name)
 }
 
-function getSchedule(name: string): Schedule {
+function getSchedule(name: string): Schedule | undefined {
   return schedules.get(name);
 }
 
-function isRunningSchedule(name:string) : boolean {
-  return schedules.get(name)?.isRunning
+function isRunningSchedule(name: string): boolean {
+  return schedules.get(name)?.isRunning ? true : false
 }
 
-function runSchedule(name: string,initRun=true):void {
+function runSchedule(name: string, initRun = true): void {
   const schedule = getSchedule(name)
   if (!schedule) {
     console.warn('not register this schedule')
     return
   }
-  const {isRunning,instance,fun,interval} = schedule
+  const { isRunning, instance, fun, interval } = schedule
   if (isRunning && instance) {
     instance.clear()
   }
   if (initRun) {
     fun()
   }
-  schedules.set(name,{
+  schedules.set(name, {
     ...schedule,
     instance: EpSetInterval(() => {
       fun()
-    },interval),
-    isRunning:true
+    }, interval),
+    isRunning: true
   })
 }
 
-function stopSchedule(name:string):void {
+function stopSchedule(name: string): void {
   if (!hasSchedule(name)) {
     return
   } else {
-   const thisSchedule = getSchedule(name)
-   const {instance} = thisSchedule
-   if (instance) {
-     instance.clear()
-   }
-   schedules.set(name,{
-    ...thisSchedule,
-    instance: null,
-    isRunning:false
-  })
+    const thisSchedule = getSchedule(name)
+    if (thisSchedule?.instance) {
+      thisSchedule.instance.clear()
+      schedules.set(name, {
+        ...thisSchedule,
+        instance: null,
+        isRunning: false
+      })
+    }
+
   }
 }
 
-function clearSchedule(name:string):void {
+function clearSchedule(name: string): void {
   if (!hasSchedule(name)) {
     return
   } else {
-   const thisSchedule = getSchedule(name)
-   const {instance} = thisSchedule
-   if (instance) {
-     instance.clear()
-   }
-   schedules.delete(name)
+    const thisSchedule = getSchedule(name)
+    if (thisSchedule?.instance) {
+      thisSchedule.instance.clear()
+    }
+    schedules.delete(name)
   }
 }
 
-function EpSetInterval(callback:any, delay:number) {
+function EpSetInterval(callback: any, delay: number) {
   let timeoutId = setTimeout(function repeat() {
     callback();
     timeoutId = setTimeout(repeat, delay);
@@ -105,11 +104,11 @@ function EpSetInterval(callback:any, delay:number) {
   };
 }
 
-function clearAllSchedule ():void {
+function clearAllSchedule(): void {
   const schedulesArray = Array.from(schedules.values());
   schedulesArray.forEach(schedule => {
     if (schedule.instance) {
-      schedule.instance.clear(); 
+      schedule.instance.clear();
     }
   })
   schedules.clear()
